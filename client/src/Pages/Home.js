604bf8ebbca2a38af0104113609773997602f420
IN-Accurate <vishalsankar410@gmail.com>
2023-04-04 15:05:44 +0530

    Fix Sidebar

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -7,7 +7,17 @@ function Home() {
       <div className='home-top'>
         <Navbar />
       </div>
-      <div className='home-bot'>HOME</div>
+      <div className='home-bot' id='home-bot'>
+        <div className='home-bot-left' id='home-bot-left'>
+          LEFT
+        </div>
+        <div className='home-bot-right'>
+          RIGHT
+          <div className='home-bot-right-1'></div>
+          <div className='home-bot-right-2'></div>
+          <div className='home-bot-right-2'></div>
+        </div>
+      </div>
     </div>
   );
 }

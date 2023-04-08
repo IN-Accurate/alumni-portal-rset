648e43592ecfaf45408b5b1836b32183cd5c2459
IN-Accurate <vishalsankar410@gmail.com>
2023-04-08 19:33:55 +0530

    Add some event-cont content

--- client/src/Pages/Events.js
+++ client/src/Pages/Events.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
+import { FaChevronCircleDown } from 'react-icons/fa';
+import '../CSS/Events.css';
 
 function Events() {
-  return <div>Events</div>;
+  let event_data = ['event 1', 'event 2', 'event 3'];
+  const [showAllEvents, setShowAllEvents] = useState(false);
+
+  const toggleShowAllEvents = () => {
+    setShowAllEvents(!showAllEvents);
+  };
+
+  return (
+    <div className='event-container'>
+      <div className='event-hero' onClick={toggleShowAllEvents}>
+        Events
+        <span className='event-icon' onClick={toggleShowAllEvents}>
+          <FaChevronCircleDown
+            className={
+              showAllEvents
+                ? 'rotate-icon rotate-both-icon '
+                : 'rotate-both-icon '
+            }
+          />
+        </span>
+      </div>
+      <div className={`event-list ${showAllEvents ? 'event-list-show' : ''}`}>
+        {event_data.map((data, index) => (
+          <div key={index}>{data}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Events;
